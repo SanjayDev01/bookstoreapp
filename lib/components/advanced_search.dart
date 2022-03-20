a959c32747f9afd03039d39b9fe6a7fdcee11db7
Sanju Poojari <36479146+SanjayDev01@users.noreply.github.com>
2022-03-20 23:19:06 +0530

    addvanced search done

--- lib/components/advanced_search.dart
+++ lib/components/advanced_search.dart
@@ -1,4 +1,7 @@
+import 'package:bookstoreapp/components/books_detail.dart';
+import 'package:bookstoreapp/models/books.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AdvancedSearch extends StatefulWidget {
   const AdvancedSearch({Key? key}) : super(key: key);
@@ -8,8 +11,114 @@ class AdvancedSearch extends StatefulWidget {
 }
 
 class _AdvancedSearchState extends State<AdvancedSearch> {
+  final _formKey = GlobalKey<FormState>();
+  List<Book> newBookList = [];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                //search boc
+                Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Search",
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                      inputFormatters: [
+                        LengthLimitingTextInputFormatter(30),
+                      ],
+                      onChanged: (value) {
+                        print(value);
+                        setState(() {
+                          newBookList = [];
+                          for (var i = 0; i < booklist.books.length; i++) {
+                            if (booklist.books[i].title
+                                .toLowerCase()
+                                .contains(value.toLowerCase())) {
+                              newBookList.add(booklist.books[i]);
+                            }
+                          }
+                        });
+                      },
+                      onSaved: (value) {
+                        print(value);
+
+                        setState(() {
+                          newBookList = [];
+                          for (var i = 0; i < booklist.books.length; i++) {
+                            if (booklist.books[i].title
+                                .toLowerCase()
+                                .contains(value!.toLowerCase())) {
+                              newBookList.add(booklist.books[i]);
+                            }
+                          }
+                        });
+                      },
+                      onFieldSubmitted: (value) {
+                        print(value);
+
+                        setState(() {
+                          newBookList = [];
+                          for (var i = 0; i < booklist.books.length; i++) {
+                            if (booklist.books[i].title
+                                .toLowerCase()
+                                .contains(value.toLowerCase())) {
+                              newBookList.add(booklist.books[i]);
+                            }
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.70,
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: newBookList.length,
+                    itemBuilder: (BuildContext context, int i) {
+                      // ignore: unnecessary_null_comparison
+                      if (booklist.books.isNotEmpty) {
+                        return BooksDetails(
+                          title: newBookList[i].title,
+                          author: newBookList[i].author,
+                          cover: newBookList[i].cover,
+                          price: newBookList[i].price,
+                          isFav: false,
+                          isCart: false,
+                        );
+                      } else {
+                        return Container();
+                      }
+                    },
+                  ),
+                ),
+              ],
+            )),
+      ),
+    );
   }
 }
